112d99cb739b3438a0c615656b2209293be748fe
wombat1742 <tomgogard@gmail.com>
2023-05-26 19:32:54 +0300

    FirstBackendCHanges

---
+++ app/Http/Controllers/ChatGptController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;  
+
+class ChatGptController extends Controller
+{
+    public function index(): JsonResponse
+
+    {
+        $search = "laravel get ip address";
+        $data = Http::withHeaders([
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer '.env('OPENAI_API_KEY'),
+                  ])
+                  ->post("https://api.openai.com/v1/chat/completions", [
+                    "model" => "gpt-3.5-turbo",
+                    'messages' => [
+                        [
+                           "role" => "user",
+                           "content" => $search
+                       ]
+
+                    ],
+                    'temperature' => 0.5,
+                    "max_tokens" => 200,
+                    "top_p" => 1.0,
+                    "frequency_penalty" => 0.52,
+                    "presence_penalty" => 0.5,
+                    "stop" => ["11."],
+                  ])
+                  ->json();
+        return response()->json($data['choices'][0]['message'], 200, array(), JSON_PRETTY_PRINT);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatGptController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('open-ai', [ChatGptController::class, 'index']);
