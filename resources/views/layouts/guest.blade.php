59dd6de5c9b4ed09c934afc10925e55866faff03
Kalio <homut483@gmail.com>
2023-05-28 11:56:31 +0300

    register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -28,6 +28,7 @@
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    
                 </a>
             </div>
 
